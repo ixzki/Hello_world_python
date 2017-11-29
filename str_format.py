0268636134d80dc7f7f6f670b80a506f3d8933bf
ixzki <240771279@qq.com>
2017-11-29 21:12:06 +0800

    about python

---
+++ str_format.py
@@ -0,0 +1,16 @@
+age=20
+name='Samuel'
+
+print('{0} was {1} years old when he wrote this book'.format(name,age))
+print('Why is {0} playing with that python?'.format(name))
+
+print('{0:.3f}'.format(1.0/3))
+print('{0:_^11}'.format('hello'))
+print('{name} wrote {book}'.format(name='Swaroop',book='A Byte of Python'))
+
+print('a',end='')
+print('b')
+print('a',end=' ')
+print('b')
+
+r"Newlines are indicated by \n"
---
+++ var.py
@@ -0,0 +1,8 @@
+i=5
+print(i)
+i=i+1
+print(i)
+
+s='''This is a multi-ine string.
+This is the secong line.'''
+print(s)
